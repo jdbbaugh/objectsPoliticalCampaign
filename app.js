25ec52e91ee744ea01a9c565ec0da2e7c5516c3d
jdbbaugh <jbaugh78@msn.comgit>
2018-11-26 10:46:15 -0600

    build initial campaign app object

---
+++ app.js
@@ -0,0 +1,33 @@
+const sangerApp = {
+    congressionalDistrict: "5th Tennessee District",
+    platformStatements: {
+        taxes: "Dont't like them",
+        jobs: 'Need more of them',
+        infrastructure: 'is cool',
+        healthCare: 'Make it less pricey',
+        crimeEnforcement: 'Monopoly go to jail'
+    },
+    donateHere: 'www.paypal.com',
+    calendarEvents: {
+        nov22: 'Thanksgiving',
+        dec12: 'voting',
+        dec25: 'Presents'
+    },
+    volunteerInfo: {
+        name: '',
+        address: '',
+        email: '',
+        phone: '',
+        availability: '',
+        desiredActivity: ['Answering Phone Calls', 'taking polls', 'making signs', 'etc...']
+    },
+    biography: 'Insert epic Bio!',
+    imgGallery: {
+        headShot: 'https://www.example.com/images/img123.jpg',
+        familyPic: 'https://www.example.com/images/img1234.jpg',
+        constituentPic: 'https://www.example.com/images/img12345.jpg'
+    },
+    missionStatement: 'eat tacos',
+    registerToVoteURL: 'https://www.usa.gov/register-to-vote'
+
+}
